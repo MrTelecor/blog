c159debf5dc4a1dfd1eab89c8a56695f6ea03594
Iker Lario <iklaflo@gmail.com>
2024-12-04 03:42:54 +0100

    security alert fix

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
  
 const firebaseConfig = {
-    apiKey: "AIzaSyC3GJbFJZyIK73BSn3_sXqA2oeLFgEF-3w",
-    authDomain: "blogapp-8d3f0.firebaseapp.com",
-    projectId: "blogapp-8d3f0",
-    storageBucket: "blogapp-8d3f0.firebasestorage.app",
-    messagingSenderId: "266183192379",
-    appId: "1:266183192379:web:48f825529acc57b886d11e"
+    apiKey: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    authDomain: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    projectId: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    storageBucket: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    messagingSenderId: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    appId: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 };
  
 const app = initializeApp(firebaseConfig);
